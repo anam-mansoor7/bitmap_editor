8f4c8f526c7eef7ba43ed9a1326bd9be7f7385d7
anam <anam.mansoor@7vals.com>
2016-04-28 14:49:20 +0100

    image should exist before S, C command and add range validation to I command

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -9,7 +9,7 @@ class BitmapEditor
 # Maybe put a color is invalid error
   def run
     @running = true
-    image = []
+    @image = []
     puts 'type ? for help'
     while @running
       print '> '
@@ -21,21 +21,21 @@ class BitmapEditor
         when 'X'
           exit_console
         when 'S'
-          image.print
+          @image.print if image_exists?
         when 'C'
-          image.clear_bitmap 
+          @image.clear_bitmap if image_exists?
         when /I (\d) (\d)\z/ 
           #I M N - Create a new M x N image
-          image = Image.new(params[3], params[2])
+          create_image(params[2], params[1])
         when /L (\d) (\d) ([A-Z])\z/
           #L X Y C - Colours the pixel (X,Y) with colour C  
-          image.color_pixel(params[3], params[2], params[4])
+          @image.color_pixel(params[3], params[2], params[4])
         when /V (\d) (\d) (\d) ([A-Z])\z/ 
           #V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2
-          image.draw_vertical_segment(params[2], params[3], params[4], params[5])
+          @image.draw_vertical_segment(params[2], params[3], params[4], params[5])
         when /H (\d) (\d) (\d) ([A-Z])\z/
           # H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2
-          image.draw_horizental_segment(params[2], params[3], params[4], params[5])          
+          @image.draw_horizental_segment(params[2], params[3], params[4], params[5])          
         else
           puts 'unrecognised command :('
       end
@@ -59,12 +59,30 @@ S - Show the contents of the current image
 X - Terminate the session"
     end
 
+    def create_image(row, col)
+      if valid_bounds?(params[2], params[3])
+        @image = Image.new(params[3], params[2])
+      else
+        puts "Error: Row and column needs to be between 1 and 250"
+      end 
+    end
+
     def parse_input(input)
       input.chomp.split(' ').map do |x| 
         is_capital_letter(x) ? x : x.to_i - 1 
       end   
     end
 
+    def image_exists?
+      error_message = "Error: Image does not exist. Create it using I command"  
+      @image.is_a?(Image) ? true : puts error_message
+    end
+
+    def validate_range?(row, col)
+      error_message = "Error: The range must be between 1 and 250"  
+      (row.between?(0, 249) && col.between?(0, 249)) ? true : 
+    end
+    
     def is_capital_letter(char)
       !((char =~ /^[A-Z]$/).nil?)
     end
